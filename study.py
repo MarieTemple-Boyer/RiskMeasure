6d2f4f20944c995cb2b2d27e8b60d602c2f1ce5f
Marie Temple-Boyer <marie.temple-boyer@edf.fr>
2024-12-16 16:28:07 +0100

    etude

--- study.py
+++ study.py
@@ -5,13 +5,14 @@ import openturns as ot
 import openturns.viewer as viewer
 import numpy as np
 import matplotlib.pyplot as plt
-from riskmeasure import superquantile#, superquantile_labopin, superquantile_min
+# , superquantile_labopin, superquantile_min
+from riskmeasure import superquantile
 
 
-NUMBER_OF_SAMPLES = 1000
+# NUMBER_OF_SAMPLES = 10000
+NUMBER_OF_SAMPLES = 500
 SAMPLE_SIZES = [10, 100, 500, 1000]
 
-#METHODS = [superquantile, superquantile_labopin, superquantile_min]
 METHODS_NAME = ['standard',    'labopin',             'minimization']
 
 SAMPLE_SIZE = 100
@@ -66,6 +67,8 @@ estimator_id = np.zeros((len(METHODS_NAME), len(SAMPLE_SIZES)))
 estimator_value = []
 
 bias = np.zeros((len(METHODS_NAME), len(SAMPLE_SIZES)))
+err = np.zeros((len(METHODS_NAME), len(SAMPLE_SIZES)))
+var = np.zeros((len(METHODS_NAME), len(SAMPLE_SIZES)))
 
 """ FAIRE DES CLASSES ? """
 """ IL FAUT METTRE L'ECART-TYPE DIVISÉ PAR L'ESPERANCE """
@@ -76,16 +79,18 @@ for (i, method) in enumerate(METHODS_NAME):
     for (j, n) in enumerate(SAMPLE_SIZES):
         estim0 = get_superquantile_estimators(
             ALPHA, n, NUMBER_OF_SAMPLES, method=method)
-        estimator_id[i,j] = len(estimator_value)
+        estimator_id[i, j] = len(estimator_value)
         estimator_value.append(estim0)
         bias0, var0, err0 = bias_variance(ALPHA, estim0)
-        bias[i, j] = np.log(err0)
+        bias[i, j] = bias0
+        err[i, j] = err0
+        var[i, j] = var0
 
 for (i, method_name) in enumerate(METHODS_NAME):
-    plt.plot(SAMPLE_SIZES, bias[i], '-o', label=method_name)
+    plt.loglog(SAMPLE_SIZES, err[i], '-o', label=method_name)
 
 plt.xlabel('Sample size')
+plt.ylabel('Quadratic error of the estimator of the superquantile')
 plt.legend()
-plt.title("Bias of the estimators")
 
 plt.show()
